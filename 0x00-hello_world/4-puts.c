6658a81013a22f79bec00c9f803c768418d4c5bf
Yonas Dessie <Yonasdessie034@gmail.com>
2022-12-09 23:28:59 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: 0
- */
+ * main - Entry
+ * Return: always 0
+ * /
 
 int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-
-}	
+{ 
+         puts("\"Programming is like building a multilingual puzzle");
+	 return (0);
+}
