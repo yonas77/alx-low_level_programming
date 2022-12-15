730905c16aa1979bd47aa17a2c10d02127ccee9e
Yonas Dessie <Yonasdessie034@gmail.com>
2022-12-15 08:39:46 -0800

    task3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,7 +9,7 @@
  * Return: void
  */
 
-void print_remaining_days(int month, int day, int  int year)
+void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
