79ee3896c38dfe0672adcd07435974f35f40b925
Yonas Dessie <Yonasdessie034@gmail.com>
2022-12-21 08:58:54 -0800

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,7 +15,7 @@ char *_strcat(char *dest, char *src)
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
-		dext_len++;
+		dest_len++;
 
 	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
