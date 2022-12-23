ebda4fc49db3af26e9b78f277f1fed02c5f1806f
Yonas Dessie <Yonasdessie034@gmail.com>
2022-12-23 10:32:10 -0800

    task 9

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -4,7 +4,6 @@
 /**
  * rot13 - encoder rot13
  * @s: pointer to string params
- *
  * Return: *s
  */
 
@@ -12,15 +11,15 @@ char *rot13(char *s)
 {
 	int i;
 	int j;
-
-	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
+	char data1[] =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] =
+		"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
 		{
-			if (s[i] == datarot[j])
+			if (s[i] == data1[j])
 			{
 				s[i] = datarot[j];
 				break;
