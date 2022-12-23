d708a657d858cbc8f6a9f5b5aee019af7869d493
Yonas Dessie <Yonasdessie034@gmail.com>
2022-12-23 10:49:03 -0800

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -13,12 +13,12 @@ void print_number(int n)
 	n1 = n;
 	if (n < 0)
 	{
-		_putchar('-');
+		putchar('-');
 		n1 = -n;
 	}
 	if (n1 / 10 != 0)
 	{
-		pint_number(n1 / 10);
+		print_number(n1 / 10);
 	}
 	_putchar((n1 % 10) + '0');
 }
