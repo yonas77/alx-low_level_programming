21f942901ea416c96719e8208b38681c0fe5539a
Yonas Dessie <Yonasdessie034@gmail.com>
2023-01-18 09:04:16 -0800

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+/**
+ * print_namae - prints a name.
+ * @name: pointer to name.
+ * @f: function pointer.
+ *
+ * Return: void.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
